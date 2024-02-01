3a948ada047beb1de1748a30c9195878c305e102
WesleySarmento <wesleys.494@gmail.com>
2024-01-31 22:17:19 -0300

    Update atualização de aula Map

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
-let myLi = ''
+const buttonMapAll = document.querySelector('.map-all')
 
 
-function showAll(){
-    menuOptions.forEach( products => {
+
+function showAll(productsArray){
+    myLi = ''
+
+    productsArray.forEach( products => {
         myLi = myLi +
             `
             <li>
                 <img src=${products.src}>
                 <p>${products.name}</p>
-                <p class="item-price">${products.price}</p>
+                <p class="item-price">R$ ${products.price}</p>
             </li>
             `
         
@@ -18,5 +21,13 @@ function showAll(){
         list.innerHTML = myLi
 }
 
+function mapAllItems(){
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9, //Dar 10% de desconto
+    }))
+    showAll(newPrices)
+}
 
-buttonShowAll.addEventListener('click', showAll)
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
